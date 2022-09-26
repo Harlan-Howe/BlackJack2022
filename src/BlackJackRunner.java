ac2e84c85c21d6a0d41488709c160cd4c4c7ed97
Harlan Howe <hhowe7@gatech.edu>
2022-09-26 08:50:11 -0500

    Added local exampleDeck variable to testDealerAndHuman(). Documented method for testDealerAndHuman(). Added comment to end of call to testDealerAndHuman() to comment it out in final version.

--- src/BlackJackRunner.java
+++ src/BlackJackRunner.java
@@ -3,15 +3,21 @@ public class BlackJackRunner
     public static void main(String[] args)
     {
         System.out.println("----------------------------  Temporary Test code....");
-        testDealerAndHuman();
+        testDealerAndHuman(); // comment this out for the final project.
 
         System.out.println("----------------------------  The game starts here.");
         Referee ref = new Referee();
         ref.play();
     }
 
+    /**
+     * Run any tests you have on example Dealer or Humans
+     */
     public static void testDealerAndHuman()
     {
+
+        CardPile exampleDeck = CardPile.makeDeck(CardPile.ACE_LOW); // in case you find it handy....
+
         System.out.println("Testing Dealer...");
         // add code here to test the dealer is working correctly.
 
