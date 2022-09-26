c83cfa6a7344462208eb12e9799a8122789db841
Harlan Howe <hhowe7@gatech.edu>
2022-09-26 09:33:12 -0500

    documented stub functions. Wrote most of getBet.

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HumanPlayer
@@ -16,6 +17,10 @@ public class HumanPlayer
         name = keyReader.nextLine();
     }
 
+    /**
+     * overloaded constructor allowing to create a HumanPlayer with a given staring bankroll
+     * @param startBalance - the amount of money the player starts with.
+     */
     public HumanPlayer(double startBalance)
     {
         this();
@@ -37,26 +42,66 @@ public class HumanPlayer
         return -1;
     }
 
+    /**
+     * requests the size of the bet for the user to make.
+     * @return bet - a positive double.
+     */
     public double getBet()
     {
-        return 1;
+        boolean badAnswer = true;
+        double bet = 0;
+        while(badAnswer)
+        {
+            System.out.println("How much do you wish to bet? ");
+            try // we're about to attempt to get a number from the user, which may cause an exception....
+            {
+                bet = keyReader.nextDouble();
+                String dummy = keyReader.nextLine(); // clear the newline character in the input buffer after our number.
+            }
+            catch (InputMismatchException imExcp) // if the user's input wasn't a double...
+            {
+                System.out.println("That was not a number. Please try again.");
+                continue;
+            }
+
+            // TODO: put your own code here to check whether the user gave a valid number. If so, change badAnswer to false.
+            //   otherwise, tell the user what was wrong with his/her choice.
+        }
+        balance -= bet;
+        return bet;
     }
 
+    /**
+     * receive the given card and put it in the human player's hand.
+     * @param c - the card to receive.
+     * Precondition: c is not null.
+     */
     public void takeCard(Card c)
     {
         ;
     }
 
+    /**
+     * remove all cards from the player's hand, in preparation for another round of the game.
+     */
     public void clearHand()
     {
         ;
     }
 
+    /**
+     * ask the user whether he/she wants to hit or stay.
+     * @return 1 for hit or 0 for stay.
+     */
     public int askForHitOrStay()
     {
         return 0;
     }
 
+    /**
+     * increase our balance by the given money.
+     * @param cashWon - amount of money to add to the balance.
+     */
     public void collectWinnings(double cashWon)
     {
         ;
